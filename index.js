bf75883c9be1cc24d99a68a413c3d18b750b9ed7
eunai <41470275+eunai@users.noreply.github.com>
2020-09-24 18:32:46 -0700

    Added logo, nav, nav-links, and implemented bootstrap to index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`%c ________________________________________
+< mooooooooooooooooooooooooooooooooooooo >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`, "font-family:monospace")
